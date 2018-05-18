068783ba0c5bab3f1deefc36d8225e3e071af907
Giulia Mueller <giulia_mueller@yahoo.com>
2018-05-17 21:22:53 -0400

    OtherBases

---
+++ OtherBases.java
@@ -0,0 +1,18 @@
+package chapter1;
+
+public class OtherBases {
+
+	public static void main(String[] args) {
+		
+		int decimal = 123;		// base10
+		int binary  = 0b1111011;	// base2
+		int octal  	= 0173;			// base8
+		int hexadecimal	= 0x7b;		// base16
+			
+		System.out.println("Base 10: 1_2_3 = " + decimal);		
+		System.out.println("Base 2: 0b1111011 = " + binary);	
+		System.out.println("Base 8: 0173 = " + octal);	
+		System.out.println("Base 16 0x7b = " + hexadecimal);	
+
+	}
+}
